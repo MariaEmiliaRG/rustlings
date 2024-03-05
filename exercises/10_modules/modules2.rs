19009ddced3916f42e62e367a86f312298ac77be
MariaEmiliaRG <mariaemiliaramirezgomez@comunidad.unam.mx>
2024-03-04 19:08:41 -0600

    Feat: ejercicos modulos 9 - 10

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -7,28 +7,29 @@
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 mod delicious_snacks {
-    // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    // Corregir los 'use' statements para importar y asignar alias a las constantes
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
+        // Hacer las constantes públicas para que puedan ser accesibles fuera del módulo
         pub const PEAR: &'static str = "Pear";
         pub const APPLE: &'static str = "Apple";
     }
 
     mod veggies {
+        // Hacer las constantes públicas para que puedan ser accesibles fuera del módulo
         pub const CUCUMBER: &'static str = "Cucumber";
         pub const CARROT: &'static str = "Carrot";
     }
 }
 
 fn main() {
+    // Acceder a las constantes utilizando los alias
     println!(
         "favorite snacks: {} and {}",
         delicious_snacks::fruit,
         delicious_snacks::veggie
     );
-}
+} 
